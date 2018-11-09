4b15a0a2089db0b8e7189503ca8dc1148a25bda9
KingCarnival <pb.itoldyouso@gmail.com>
2018-11-08 21:12:04 -0500

    Datasets

--- R/Scripts/Lesson 2.R
+++ R/Scripts/Lesson 2.R
@@ -1,1 +1,11 @@
 library(tidyverse)
+
+#https://www.kaggle.com/kemical/kickstarter-projects#ks-projects-201801.csv
+kickstarterTable<-read.csv("Datasets/Kickstarters/ks-projects-201801.csv")
+
+
+musicTable<-filter(kickstarterTable, category == "Music")
+successMusicTable<-filter(musicTable, state == "successful")
+
+successMusicPlot<-ggplot(successMusicTable, aes(successMusicTable$goal,successMusicTable$pledged))+geom_point()
+print(successMusicPlot)
