606402e52faa133b69b7fd6a246379b2b69d38e5
KingCarnival <pb.itoldyouso@gmail.com>
2018-11-09 17:47:01 -0500

    fixea

--- R/Scripts/Lesson 2.R
+++ R/Scripts/Lesson 2.R
@@ -3,9 +3,19 @@ library(tidyverse)
 #https://www.kaggle.com/kemical/kickstarter-projects#ks-projects-201801.csv
 kickstarterTable<-read.csv("Datasets/Kickstarters/ks-projects-201801.csv")
 
+ksPlot<-ggplot(kickstarterTable, aes(kickstarterTable$goal,kickstarterTable$pledged))+geom_point()
+print(ksPlot)
 
 musicTable<-filter(kickstarterTable, category == "Music")
+musicPlot <-ggplot(musicTable, aes(musicTable$goal,musicTable$pledged))+geom_point()
+
 successMusicTable<-filter(musicTable, state == "successful")
 
 successMusicPlot<-ggplot(successMusicTable, aes(successMusicTable$goal,successMusicTable$pledged))+geom_point()
 print(successMusicPlot)
+
+pdf("Results/lesson2Graphs.pdf")
+print(ksPlot)
+print(musicPlot)
+print(successMusicPlot)
+dev.off()
